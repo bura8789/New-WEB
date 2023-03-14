14eb1bf781b4d1dc051607e1776e8eb287e99468
bura8789 <125795064+bura8789@users.noreply.github.com>
2023-03-14 21:15:50 +0500

    Add files via upload

---
+++ src/Navbar.jsx
@@ -0,0 +1,37 @@
+import { Nav, NavItem, NavLink } from "reactstrap";
+import { Link } from "react-router-dom";
+function Navbar() {
+  return (
+    <div>
+      <Nav pills id="navbar-container">
+        <NavItem id="items">
+          <img src="https://adoric.com/blog/wp-content/uploads/2020/12/Nike-slogan.jpg"/>
+        
+          <Link to={'/science'} className="link">
+            Scince
+          </Link>
+          <Link to={'/cartoons'}className="link">
+            Cartoons
+          </Link>
+          <Link to={'/popular'} className="link">
+            Popular
+          </Link>
+          <Link to={'/trending'} className="link">
+            Trending
+          </Link>
+          <Link to={'/fantasy'} className="link">
+            Fantasy
+          </Link>
+          <Link to={'/war-movies'} className="link">
+            War
+          </Link>
+          <Link  className="login">
+            Login
+          </Link>
+        </NavItem>
+      </Nav>
+    </div>
+  )
+}
+
+export default Navbar;
